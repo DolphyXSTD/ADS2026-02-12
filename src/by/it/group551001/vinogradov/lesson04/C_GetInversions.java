866a21ed4dfc4d55bfe9d969e78c761a925729dd
DolphyXSTD <timofey31052008@gmail.com>
2026-03-27 09:48:21 +0300

    Task C done (lesson 4)

--- src/by/it/group551001/vinogradov/lesson04/C_GetInversions.java
+++ src/by/it/group551001/vinogradov/lesson04/C_GetInversions.java
@@ -42,6 +42,55 @@ public class C_GetInversions {
         //long finishTime = System.currentTimeMillis();
         System.out.print(result);
     }
+    int merge(int[] s, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int cnt = 0;
+        int[] left = new int[n1]; int[] right = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            left[i] = s[l + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            right[i] = s[m + 1 + i];
+        }
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while ((i < n1) && (j < n2)) {
+            if (left[i] <= right[j]) {
+                s[k] = left[i];
+                i++;
+            }
+            else {
+                cnt+= n1 - i;
+                s[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            s[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            s[k] = right[j];
+            j++;
+            k++;
+        }
+        return cnt;
+    }
+
+    int mergeSort(int[] s, int l, int r) {
+        int cnt = 0;
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            cnt += mergeSort(s, l, m);
+            cnt += mergeSort(s, m + 1, r);
+            cnt += merge(s, l, m, r);
+        }
+        return cnt;
+    }
 
     int calc(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
@@ -54,11 +103,8 @@ public class C_GetInversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int result = 0;
-        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
-
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        return mergeSort(a, 0, n-1);
     }
 }
