b7c3fcf212e906b2cd2048812edcf387e16238ba
DolphyXSTD <timofey31052008@gmail.com>
2026-04-16 14:43:39 +0300

    Task C done (lesson 7)

--- src/by/it/group551001/vinogradov/lesson07/C_EditDist.java
+++ src/by/it/group551001/vinogradov/lesson07/C_EditDist.java
@@ -49,12 +49,67 @@ import java.util.Scanner;
 public class C_EditDist {
 
     String getDistanceEdinting(String one, String two) {
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        int n = one.length(), m = two.length();
+        int[][] dp = new int[n + 1][m + 1];
+        char[][] choice = new char[n + 1][m + 1];
 
+        // Initialize borders
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+            choice[i][0] = 'D';
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+            choice[0][j] = 'I';
+        }
+        choice[0][0] = 'S';
 
-        String result = "";
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int cost = (one.charAt(i - 1) == two.charAt(j - 1)) ? 0 : 1;
+                int del = dp[i - 1][j] + 1;
+                int ins = dp[i][j - 1] + 1;
+                int rep = dp[i - 1][j - 1] + cost;
+                if (del <= ins && del <= rep) {
+                    dp[i][j] = del;
+                    choice[i][j] = 'D';
+                } else if (ins <= del && ins <= rep) {
+                    dp[i][j] = ins;
+                    choice[i][j] = 'I';
+                } else {
+                    dp[i][j] = rep;
+                    choice[i][j] = (cost == 0) ? 'M' : 'R';
+                }
+            }
+        }
+        StringBuilder rev = new StringBuilder();
+        rev.append(",");
+        int i = n, j = m;
+        while (i > 0 || j > 0) {
+            char op = choice[i][j];
+            switch (op) {
+                case 'D':
+                    rev.append(",").append(one.charAt(i - 1)).append("-");
+                    i--;
+                    break;
+                case 'I':
+                    rev.append(",").append(two.charAt(j - 1)).append("+");
+                    j--;
+                    break;
+                case 'R':
+                    rev.append(",").append(two.charAt(j - 1)).append("~");
+                    i--;
+                    j--;
+                    break;
+                case 'M':
+                    rev.append(",#");
+                    i--;
+                    j--;
+                    break;
+            }
+        }
+        String result = rev.reverse().toString();
+        return result.substring(0, result.length()-1);
     }
 
 
